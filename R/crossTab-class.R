6e6abb583f33e11f2f43b09b64fa2fb987e6ef33
Anamaria Elek <anamariaelek@gmail.com>
2018-02-28 17:18:18 +0100

    Enrichment analysis S4

---
+++ R/crossTab-class.R
@@ -0,0 +1,144 @@
+#' @include enrichment.R
+NULL
+
+#' An S4 class \code{crossTab}
+#'
+#' A contingency table that displays...
+#'
+#' @slot genes A character vector of sequences annotations (KO, COG).
+#' @slot variable A coresponding numeric vector of CU values.
+#' @slot table A contingecy table.
+#'
+setClass(
+    "crossTab",
+    slots = c(
+        genes = "character",
+        variable = "numeric",
+        table = "data.table"
+    )
+)
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### crossTab constructor
+###
+
+#' @rdname crossTab-class
+#' @export
+setGeneric(
+    name = "crossTab",
+    def = function(genes, variable, ...){
+        standardGeneric("crossTab")
+    }
+)
+
+#' @describeIn crossTab Create new objects of class \code{crossTab}.
+#'
+#' Creates a contingency table for the list of annotated sequences and
+#' the corresponding list of codon usage (CU) values.
+#'
+#' @param genes A character vector of sequences annotations (KO, COG).
+#' @param variable A coresponding numeric vector of CU values.
+#' @param threshold A threshold value (or a vector of values) of the variable.
+#'    Sequences with value of the given variable greater than threshold are
+#'    counted as a subset. Default is 1.
+#' @param percentiles A single value or a vector of values between 0 and 1.
+#'    Sequences with value of the given variable in the top percentiles are
+#'    counted as a subset.
+#'
+#' @return Returns a data.table with category values in rows, and with separate
+#'    columns for counts in background (all) and subsets, i.e. for diferrent
+#'    thresholds/percentiles provided.
+#'
+#' @export
+setMethod(
+    f = "crossTab",
+    signature = c(genes = "character", variable = "numeric"),
+    definition = function(genes, variable, threshold = 1L, percentiles = NULL) {
+        new("crossTab",
+            genes = genes,
+            variable = variable,
+            table = make.contable(genes, variable, threshold, percentiles))
+    }
+)
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### reduce crossTab
+###
+
+#' Reduce objects of \code{crossTab} class.
+#'
+#' @export
+setGeneric(
+    name = "reduceCrossTab",
+    def = function(x, target){
+        standardGeneric("reduceCrossTab")
+    }
+)
+#' @describeIn reduceCrossTab Reduce \code{crossTab}.
+#'
+#' Reduce the input contingency table by associating genes with KEGG Pathway
+#' or KEGG Module identifiers.
+#'
+#' @param x A \code{crossTab} object to be reduced.
+#' @param target A character vector indicating which onthology to use, either
+#'    \code{"pathway"} or \code{"module"}.
+#'
+#' @return Returns input \code{crossTab} object, with updated contingency table,
+#'    displaying new category values in rows, and updated counts in columns.
+#'
+#' @export
+#'
+setMethod(
+    f = "reduceCrossTab",
+    signature = c("crossTab", "character"),
+    definition = function(x, target){
+        new("crossTab",
+            genes = x@genes,
+            variable = x@variable,
+            table = reduce.contable(x@table, target))
+    }
+)
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Enrichment analysis
+###
+
+#' Enrichment analysis for object \code{crossTab} class.
+#' @export
+setGeneric(
+    name = "enrichment",
+    def = function(x, ...){
+        standardGeneric("enrichment")
+    }
+)
+#' @describeIn enrichment Enrichment analysis.
+#'
+#' @param x A \code{crossTab} object.
+#' @param target A character vector indicating which onthology to use, either
+#'    \code{"pathway"} or \code{"module"}.
+#'
+#' @return  Returns a list of data.tables, each containing categories in rows,
+#'    and the following columns:
+#'    \itemize{
+#'      \item background counts
+#'      \item counts for a given subset
+#'      \item enrichment, calculated as the ratio:
+#'          (scaled sample counts - scaled backg. counts) / scaled backg. counts * 100,
+#'          where scaling means that sample counts are simply increased by 1,
+#'          and background counts are multiplied by ratio of summed sample counts
+#'          and summed backgroun counts, and also increased by 1.
+#'      \item M, log ratios of scaled counts
+#'      \item A, mean average of scaled counts
+#'      \item pvals, p values for exact binomial test
+#'      \item padj, p values corrected by BH method.
+#'    }
+#'
+#' @export
+#'
+setMethod(
+    f = "enrichment",
+    signature = c("crossTab"),
+    definition = function(x, pvalueCutoff = numeric(), pAdjustMethod = "BH"){
+        .enrichment(x@table)
+    }
+)
--- R/enrichment.R
+++ R/enrichment.R
@@ -3,38 +3,18 @@
 #' @import data.table
 NULL
 
-#' Make contingency table
-#'
-#' Creates a contingency table for the list of annotated sequences and
-#' the corresponding list of codon usage (CU) values.
-#'
-#' @param geneList A character vector of sequences annotations (KO, COG).
-#' @param variable A coresponding numeric vector of CU values.
-#' @param threshold A threshold value (or a vector of values) of the variable.
-#'    Sequences with value of the given variable greater than threshold are
-#'    counted as a subset. Default is 1.
-#' @param percentiles A single value or a vector of values between 0 and 1.
-#'    Sequences with value of the given variable in the top percentiles are
-#'    counted as a subset.
-#'
-#' @return Returns a data.table with category values in rows, and with separate
-#'    columns for counts in background (all) and subsets, i.e. for diferrent
-#'    thresholds/percentiles provided.
-#'
-#' @export
-#'
-make.contable <- function(geneList, variable,
-                          threshold = 1L, percentiles = NULL) {
+make.contable <- function(genes, variable,
+                      threshold = 1L, percentiles = NULL) {
 
-  # geneList <- as.factor(slot(cTobject, category))
-  geneList <- as.factor(geneList)
-  all <- as.vector(table(geneList))
-  result <- data.table(category = levels(geneList),
+  # genes <- as.factor(slot(cTobject, category))
+  genes <- as.factor(genes)
+  all <- as.vector(table(genes))
+  result <- data.table(category = levels(genes),
                        all = all)
 
   if(!is.null(percentiles)) {
     top.perc <- lapply(percentiles, function(x) {
-      as.vector(table(geneList[variable >= quantile(variable, x)]))
+      as.vector(table(genes[variable >= quantile(variable, x)]))
     })
     names <- make.names(paste("top", percentiles, sep="_"))
     result[, (names) := top.perc]
@@ -44,7 +24,7 @@ make.contable <- function(geneList, variable,
 
   if(!is.null(threshold)) {
     top.thresh <- lapply(threshold, function(x) {
-      as.vector(table(geneList[variable >= x]))
+      as.vector(table(genes[variable >= x]))
     })
     names <- make.names(paste("gt", threshold, sep="_"))
     result[, (names) := top.thresh]
@@ -55,20 +35,6 @@ make.contable <- function(geneList, variable,
   return(result)
 }
 
-#' Reduce contingency table
-#'
-#' Given contingency table with gene identifiers, reduce the table by associating
-#' gene identifiers with either KEGG Pathway or KEGG Module identifiers.
-#'
-#' @param contable A contingency table.
-#' @param target A character vector indicating which onthology to use, either
-#'    \code{"pathway"} or \code{"module"}.
-#'
-#' @return Returns a data.table with new category values in rows, and with the same
-#'    columns as in the input contingency table.
-#'
-#' @export
-#'
 reduce.contable <- function(contable, target) {
     if (target == "pathway") {
         DT <- KO_PATHWAYS
@@ -86,31 +52,9 @@ reduce.contable <- function(contable, target) {
     rbindlist(out, idcol = "category")
 }
 
-#' Enrichment analysis
-#'
-#' Enrichment analysis...
-#'
-#' @param contable A contingency table.
-#'
-#' @return  Returns a list of data.tables, each containing categories in rows,
-#'    and the following columns:
-#'    \itemize{
-#'      \item background counts
-#'      \item counts for a given subset
-#'      \item enrichment, calculated as the ratio:
-#'          (scaled sample counts - scaled backg. counts) / scaled backg. counts * 100,
-#'          where scaling means that sample counts are simply increased by 1,
-#'          and background counts are multiplied by ratio of summed sample counts
-#'          and summed backgroun counts, and also increased by 1.
-#'      \item M, log ratios of scaled counts
-#'      \item A, mean average of scaled counts
-#'      \item pvals, p values for exact binomial test
-#'      \item padj, p values corrected by BH method.
-#'    }
-#'
-#' @export
-#'
-enrichment <- function(contable) {
+
+#' @importClassesFrom DOSE enrichResult
+.enrichment <- function(contable, pvalueCutoff = numeric(), pAdjustMethod = "BH") {
 
     rows <- names(contable)
     top_rows <- rows[grep("top|gt", rows)]
@@ -135,7 +79,7 @@ enrichment <- function(contable) {
                 b = binom.test(x[2], top.sum, x[1]/all.sum)
                 b$p.value
             })
-        padj <- p.adjust(pvals, method = "BH")
+        padj <- p.adjust(pvals, method = pAdjustMethod)
 
         ct[, ':='(enrich = (scaled_top - scaled_all) / scaled_all * 100,
                   M = log2(scaled_top) - log2(scaled_all),
@@ -143,9 +87,9 @@ enrichment <- function(contable) {
                   pvals = pvals,
                   padj = padj)]
 
+        if(length(pvalueCutoff) != 0) ct <- ct[pvals <= pvalueCutoff,]
+
+        ct
+
     }, simplify = FALSE)
-
-    #bound <- do.call(cbind, by.top)
-    #out <- data.frame(all = all, bound)
-
 }
